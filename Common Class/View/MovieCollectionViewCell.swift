ffe60d2f59f81789627cd23eccb3d117c29dae13
Atit Modi <atitmodi2011@gmail.com>
2018-02-18 19:39:38 +0530

    Application UI flow(Tab and Nav Bar) Created with custom collection view cell

---
+++ Common Class/View/MovieCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCollectionViewCell.swift
+//  TBdm
+//
+//  Created by Atit Modi on 2/17/18.
+//  Copyright © 2018 Atit Modi. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+}
